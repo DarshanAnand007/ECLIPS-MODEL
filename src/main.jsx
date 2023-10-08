53383254116003e334e5b64a90d1db393fba232a
Darshan Anand <93935699+DarshanAnand007@users.noreply.github.com>
2023-10-08 11:32:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <h1>About Eclipse</h1>
             <p>Eclipse is an astronomical event that occurs when an astronomical object is temporarily obscured, either by passing into the shadow of another body or by having another body pass between it and the viewer.</p>
             <center>
-              <button onClick={() => window.location.href = "https://www.example.com"} class="gameBtn"> Play the Eclipse Quiz Game</button>
+              <button onClick={() => window.location.href = "https://nasa-game-1-0-yl2l-000.vercel.app"} class="gameBtn"> Play the Eclipse Quiz Game</button>
             </center>          
           </div>
       </div>
